181285f0cf25bee40efccfc6c7c05602d98556a8
Joshua Groves <josh@joshgroves.com>
2017-09-24 17:02:49 -0600

    Remove bindgen as build dependency and include generated bindings

---
+++ bindings_generator/src/main.rs
@@ -0,0 +1,21 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let out_path = PathBuf::from(env::current_dir().unwrap());
+    bindgen::Builder::default()
+        .header(out_path.join("../spirv_cross/src/wrapper.hpp").to_str().unwrap())
+        .clang_args(["-x", "c++", "-std=c++14"].iter())
+        .enable_cxx_namespaces()
+        .whitelisted_function("sc_internal.*")
+        .whitelisted_type("spv::.*")
+        .bitfield_enum(".*(Mask|Flags)")
+        .whitelisted_type("spirv_cross::Resource")
+        .opaque_type("std::.*")
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file(out_path.join("../spirv_cross/src/bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
--- spirv_cross/src/lib.rs
+++ spirv_cross/src/lib.rs
@@ -3,7 +3,7 @@ mod ScInternal {
     #![allow(non_upper_case_globals)]
     #![allow(non_camel_case_types)]
     #![allow(non_snake_case)]
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+    include!(concat!("bindings.rs"));
 }
 
 macro_rules! check {
