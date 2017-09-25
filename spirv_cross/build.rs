66668518a657f30e2189e5899110403c7eadf063
Joshua Groves <josh@joshgroves.com>
2017-09-24 18:39:06 -0600

    Add C++ flag

--- spirv_cross/build.rs
+++ spirv_cross/build.rs
@@ -9,5 +9,6 @@ fn main() {
         .file("src/vendor/SPIRV-Cross/spirv_glsl.cpp")
         .file("src/vendor/SPIRV-Cross/spirv_hlsl.cpp")
         .file("src/vendor/SPIRV-Cross/spirv_msl.cpp")
+        .flag_if_supported("-std=c++14")
         .compile("spirv-cross-rust-wrapper");
 }
