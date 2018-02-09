c86c6645457c473a2a94bb384f31ee281e2afa8f
grovesNL <josh@joshgroves.com>
2018-02-08 21:33:56 -0700

    Run rustfmt

--- spirv_cross/tests/spirv_tests.rs
+++ spirv_cross/tests/spirv_tests.rs
@@ -38,18 +38,18 @@ fn ast_gets_shader_resources() {
     assert!(
         stage_inputs
             .iter()
-            .any(|stage_input| { stage_input.name == "a_normal" })
+            .any(|stage_input| stage_input.name == "a_normal")
     );
     assert!(
         stage_inputs
             .iter()
-            .any(|stage_input| { stage_input.name == "a_position" })
+            .any(|stage_input| stage_input.name == "a_position")
     );
     assert_eq!(stage_outputs.len(), 1);
     assert!(
         stage_outputs
             .iter()
-            .any(|stage_output| { stage_output.name == "v_normal" })
+            .any(|stage_output| stage_output.name == "v_normal")
     );
     assert_eq!(shader_resources.subpass_inputs.len(), 0);
     assert_eq!(shader_resources.storage_images.len(), 0);
@@ -149,7 +149,7 @@ fn ast_sets_member_decoration() {
         uniform_buffers[0].base_type_id,
         1,
         spirv::Decoration::Offset,
-        new_offset
+        new_offset,
     ).unwrap();
 
     assert_eq!(
