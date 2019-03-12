a91b617e92966698eaeda4c33eef502f30888b26
Joshua Groves <josh@joshgroves.com>
2019-03-12 12:58:50 -0600

    Remove default features

--- .travis/linux_gcc.sh
+++ .travis/linux_gcc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export CXX=g++-5
 
-cargo build --verbose --all
-cargo test --verbose --all
+cargo build --verbose --all --all-features
+cargo test --verbose --all --all-features
--- .travis/macos_clang.sh
+++ .travis/macos_clang.sh
@@ -2,5 +2,5 @@
 export CXX=clang++
 export MACOSX_DEPLOYMENT_TARGET=10.7
 
-cargo build --verbose --all
-cargo test --verbose --all
+cargo build --verbose --all --all-features
+cargo test --verbose --all --all-features
